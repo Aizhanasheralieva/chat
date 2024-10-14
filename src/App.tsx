1d635614824e14340dde21ebb9a82e54a00016ec
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-10-14 15:46:44 +0600

    Created interval in useEffect to get messages every 3 seconds. Also added the function fetchNewMessages so that I can get new messages from the lastDatetime of the message sent. Formatted the code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
-import Chat from "./components/Chat/Chat.tsx";
+import './App.css';
+import Chat from './components/Chat/Chat.tsx';
 
 const App = () => {
   return (
     <>
-      <Chat />
+      <Chat/>
     </>
   );
 };
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,48 +1,104 @@
-import React, { useEffect, useState } from "react";
-import { IMessage } from "../../types";
-import { createLogger } from "vite";
-import Form from "../Form/Form.tsx";
+import React, { useEffect, useState } from 'react';
+import { IMessage } from '../../types';
+import Form from '../Form/Form.tsx';
 
 const Chat = () => {
-  const url = "http://146.185.154.90:8000/messages";
+  const url = 'http://146.185.154.90:8000/messages';
   const [messages, setMessages] = useState<IMessage[]>([]);
+  let lastMessageDatetime = '';
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const fetchData = async () => {
+    try {
       const response = await fetch(url);
 
       if (response.ok) {
         const messagesFromAPI = await response.json();
         console.log(messagesFromAPI);
+
         setMessages(messagesFromAPI);
+
+        if (messagesFromAPI.length > 0) {
+          lastMessageDatetime =
+            messagesFromAPI[messagesFromAPI.length - 1].datetime;
+        }
       }
-    };
-    fetchData().catch((e) => console.error(e));
-  }, []);
+    } catch (error) {
+      console.error('There is some error in getting the messages:', error);
+    }
+  };
+
+  const fetchNewMessages = async () => {
+    if (lastMessageDatetime) {
+      try {
+        const response = await fetch(
+          'http://146.185.154.90:8000/messages?datetime=' + lastMessageDatetime,
+        );
+
+        if (response.ok) {
+          const newMessages = await response.json();
+
+          if (newMessages.length > 0) {
+            setMessages((prevMessages) => [...prevMessages, ...newMessages]);
+            lastMessageDatetime = newMessages[newMessages.length - 1].datetime;
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching new messages:', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      void fetchData();
+    }
+
+    const interval = setInterval(() => {
+      console.log('Receiving some messages every three seconds');
+      void fetchNewMessages();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [messages.length]);
 
   const sendMessage = async (author: string, message: string) => {
     try {
       const data = new URLSearchParams();
-      data.set("message", message);
-      data.set("author", author);
+      data.set('message', message);
+      data.set('author', author);
 
       const response = await fetch(url, {
-        method: "post",
+        method: 'post',
         body: data,
       });
+      if (response.ok) {
+        void fetchData();
+      } else {
+        console.error('Failed to send message.');
+      }
     } catch (error) {
-      console.error("There is an error in sending message:", error);
+      console.error('There is an error in sending message:', error);
     }
   };
-
   console.log(messages);
 
   return (
     <div>
-      {messages.map((message) => (
-        <p key={message._id}>{message.message}</p>
-      ))}
-      <Form onSendMessage={sendMessage} />
+      <Form onSendMessage={sendMessage}/>
+      {messages
+        .slice()
+        .reverse()
+        .map((message) => (
+          <div key={message._id} className="card w-75 mb-3">
+            <div className="card-body">
+              <h6 className="card-title">
+                {String(new Date(message.datetime))}
+              </h6>
+              <h4 className="card-title">{message.author} said:</h4>
+              <p className="card-text">{message.message}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
